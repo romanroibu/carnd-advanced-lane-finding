c2aaf4812229dfb9adb3c5b857b9ff525b851a7e
Roman Roibu <romanroibu@gmail.com>
2018-08-08 21:46:32 +0200

    Remove IPython dependency from Video module

--- lane_finding/video.py
+++ lane_finding/video.py
@@ -4,17 +4,8 @@ import random
 import typing as t
 import numpy as np
 from moviepy.editor import VideoFileClip
-from IPython.display import HTML
 
 class Video:
-
-    def show(fpath, width=960, height=540):
-        video_html = """
-            <video width="{width}" height="{height}" controls>
-                <source src="{src}">
-            </video>
-        """.format(src=fpath, width=width, height=height)
-        return HTML(video_html)
 
     def read(fpath):
         clip = VideoFileClip(fpath)
